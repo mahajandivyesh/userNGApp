b5c5c4f34b32270cb6c931f9679646c451c425ec
Divyesh Mahajan <mahajan.divyesh@logicoy.com>
2021-03-30 16:20:46 +0530

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,75 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'user-app';
+  
+  canLogin=true;
+  userRegisterForm = this.fb.group({
+    name:['',[Validators.required]],
+    email:['', [Validators.required, Validators.email]],
+    password : ['', [Validators.required]],
+    confirmPassword : ['', [Validators.required]],
+    address:['',[Validators.required]],
+    phNum:['',[Validators.required,Validators.pattern("^((\\+91-?)|0)?[0-9]{10}$")]]
+  },
+  {
+    validator: MustMatch('password', 'confirmPassword')
+  });
+  userLoginForm = this.fb.group({
+    email:['', [Validators.required, Validators.email]],
+    password : ['', [Validators.required]],
+   
+  });  
+  constructor(private fb:FormBuilder){
+
+  }
+
+  get loginForm(){
+    return this.userLoginForm.controls;
+  }
+
+  get registerForm(){
+    return this.userRegisterForm.controls;
+  }
+  registerUser(){
+    this.canLogin = false;
+  }
+
+  loginUser(){
+    this.canLogin = true;
+  }
+
+
+  onSubmitLogin(){
+    console.log(this.userLoginForm.value);
+  }
+
+  onSubmitReg(){
+    console.log(this.userRegisterForm.value);
+  }
+
+  resetLogin(){
+    this.userLoginForm.reset();
+  }
+
+  resetRegister(){
+    this.userRegisterForm.reset();
+  }
+}
+export function  MustMatch(controlName: string, matchingControlName: string) {
+  return (formGroup: FormGroup) => {
+      const control = formGroup.controls[controlName];
+      const matchingControl = formGroup.controls[matchingControlName];
+
+      if (matchingControl.errors && !matchingControl.errors.mustMatch) {
+          // return if another validator has already found an error on the matchingControl
+          return;
+      }
+
+      // set error on matchingControl if validation fails
+      if (control.value !== matchingControl.value) {
+          matchingControl.setErrors({ mustMatch: true });
+      } else {
+          matchingControl.setErrors(null);
+      }
+  }
 }
